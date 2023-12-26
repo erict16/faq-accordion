e150d2424daaa1444b5d9cea21586e2ec3fa004a
Eric Tan <ytan5758@uni.sydney.edu.au>
2023-12-26 14:05:10 +1100

    button function implemented

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const contentElements = document.querySelectorAll('.accordion-content');
-const triggerElements = document.querySelectorAll('.accordion-header');
+const contents = document.querySelectorAll('.accordion-content');
+const icons = document.querySelectorAll('.text-icon');
 
-
-console.log("hello");
+function buttonClick(index) {
+  var contentDisplay = contents[index].style.display;
+  if (contentDisplay === 'none' || contentDisplay === '') {
+    contents[index].style.display = 'flex';
+    icons[index].style.content = 'url("assets/images/icon-minus.svg")'
+  } else if (contentDisplay === 'flex') {
+    contents[index].style.display = 'none';
+    icons[index].style.content = 'url("assets/images/icon-plus.svg")'
+  }
+}
 
 
 
